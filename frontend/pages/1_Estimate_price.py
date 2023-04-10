81d33c79ab324628048662573e0984162be09d6c
cse-vaibhav <jainvaibhav671@gmail.com>
2023-04-10 18:53:18 +0530

    Implemented a Recommender System

--- frontend/pages/1_Estimate_price.py
+++ frontend/pages/1_Estimate_price.py
@@ -3,13 +3,32 @@ import pandas as pd
 import os
 import joblib
 
+from sklearn.preprocessing import MultiLabelBinarizer
+
+
+# MultiLabel Binarizer for categorical columns
+class CustomMultiLabelBinarizer(MultiLabelBinarizer):
+
+    def __init__(self):
+        super().__init__()
+        self.classes = None
+
+    def fit(self, y):
+        return super().fit(y)
+
+    def transform(self, y):
+        return super().transform(y)
+
+    def fit_transform(self, X, y):
+        return super().fit_transform(X)
+
+
 model = joblib.load(os.path.abspath("../model/model.obj"))
 
 
 def load_data():
     data_file = os.path.abspath("../data/preprocessed.csv")
     data = pd.read_csv(data_file, index_col=0)
-
     return data
 
 
@@ -22,16 +41,12 @@ features = {
     "Storage": st.selectbox("Storage", data.Storage.unique()),
     "OS_arch": st.selectbox("OS Architecure", data.OS_arch.unique()),
     "RAM_Type": st.selectbox("Select RAM Type", data.RAM_Type.unique()),
-    "Processor_brand": st.selectbox(
-        "Select Brand of Processor", data.Processor_brand.unique()
-    ),
 }
 
 
 def predict():
     test = [list(features.values())]
     pred = int(model.predict(test))
-
     st.success(f"Estimated Price for the chosen configuration: Rs {pred}")
 
 
---
+++ frontend/pages/2_Recommend_Laptops.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import pandas as pd
+import os
+import joblib
+
+from sklearn.preprocessing import MultiLabelBinarizer
+
+
+# MultiLabel Binarizer for categorical columns
+class CustomMultiLabelBinarizer(MultiLabelBinarizer):
+
+    def __init__(self):
+        super().__init__()
+        self.classes = None
+
+    def fit(self, y):
+        return super().fit(y)
+
+    def transform(self, y):
+        return super().transform(y)
+
+    def fit_transform(self, X, y):
+        return super().fit_transform(X)
+
+
+class Recommender:
+
+    def __init__(self, recommender):
+        self.recommender = recommender
+
+    def predict(self, data):
+        return self.recommender.predict(data)
+
+    def get_k_recommendations(self, k, cluster):
+        recommendations = data[data["cluster"] == cluster]
+        if recommendations.shape[0] > 0:
+            recommendations = recommendations.sample(k)
+        return recommendations
+
+
+def load_data():
+    data_file = os.path.abspath("../data/final.csv")
+    data = pd.read_csv(data_file, index_col=0)
+    data.Storage = data.Storage.apply(lambda x: tuple(eval(x)))
+    return data
+
+
+model = joblib.load(os.path.abspath("../model/recommender.obj"))
+data = load_data()
+
+storage_sizes = set()
+for storage in data.Storage.unique():
+    for sto in storage:
+        storage_sizes.add(sto)
+
+st.write("# Recommend Laptops")
+features = {
+    "Rating": st.number_input("Minimum Rating"),  # TODO: textbox
+    "MRP": st.number_input("Minimum MRP"),  # TODO: textbox
+    "Processor": st.selectbox("Processor", data.Processor.unique()),
+    "RAM": st.selectbox("RAM", data.RAM.unique()),
+    "OS": st.selectbox("Select OS", data.OS.unique()),
+    "Storage": (st.selectbox("Storage", storage_sizes), ),
+    "OS_arch": st.selectbox("OS Architecure", data.OS_arch.unique()),
+    "RAM_Type": st.selectbox("Select RAM Type", data.RAM_Type.unique()),
+}
+
+
+def predict():
+    test = [list(features.values())]
+    pred = int(model.predict(test))
+    recommendations = model.get_k_recommendations(5, pred)
+    recommendations = st.dataframe(recommendations)
+
+
+st.button("Estimate", on_click=predict)
